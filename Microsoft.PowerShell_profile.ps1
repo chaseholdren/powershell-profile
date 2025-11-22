23c76627e2e20ae34448def19088c2888af1d0b4
Chase Holdren <chaseholdren@users.noreply.github.com>
2025-11-22 07:58:00 -0500

    Comment out help display message
    Comment out the Write-Host command for displaying help.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -700,4 +700,4 @@ if (Test-Path "$PSScriptRoot\CTTcustom.ps1") {
     Invoke-Expression -Command "& `"$PSScriptRoot\CTTcustom.ps1`""
 }
 
-Write-Host "$($PSStyle.Foreground.Yellow)Use 'Show-Help' to display help$($PSStyle.Reset)"
+# Write-Host "$($PSStyle.Foreground.Yellow)Use 'Show-Help' to display help$($PSStyle.Reset)"
